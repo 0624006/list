7500d4d5b6bb8808f071c32f3dd8d4f2916aa077
0624006 <55527041+0624006@users.noreply.github.com>
2020-11-25 17:21:04 +0800

    Create emp_emplist.php

---
+++ ajax/emp_emplist.php
@@ -0,0 +1,71 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $action = $_POST['action'];
+    require_once('connection.php');
+
+    function sql_function($sql, $conn){     
+        $result=$conn->query($sql);
+        if (mysqli_connect_errno($result)) {die("連線失敗: " . mysqli_connect_errno($result));};
+        return $result;
+    }
+
+    /* -----check_in-----*/
+    //emp_selectAll：報到刷卡頁面顯示已報到人員名單
+    if ($action == 'emp_selectAll'){
+        $result=sql_function("SELECT * FROM emplist WHERE emp_ckin_type='N' ORDER BY emp_id DESC LIMIT 40",$conn);        
+        $rows = $result->num_rows;
+        $emp_row = array();        
+        for ($i = 0; $i < $rows; $i++) {
+            $row = mysqli_fetch_array($result);
+            array_push($emp_row, array('emp_dep'=>$row['emp_dep'],'emp_no'=>$row['emp_no'],'emp_name'=>$row['emp_name']));
+        }
+        echo json_encode($emp_row);
+    }
+    //emp_insert：報到刷卡頁面報到insert
+    if ($action == 'emp_insert'){
+        $emp_no = $_POST['emp_no'];
+        $emp_name = $_POST['emp_name'];
+        $emp_dep = $_POST['emp_dep'];
+        $result=sql_function("INSERT INTO emplist(emp_no, emp_name, emp_dep) VALUES ('$emp_no', '$emp_name', '$emp_dep')",$conn);
+    }
+
+    /* -----check_in_input-----*/
+    //emp_selectALL_input：手動報到頁面顯示已手動報到人員名單
+    if ($action == 'emp_selectALL_input'){    
+        $result=sql_function("SELECT * FROM emplist WHERE emp_ckin_type='F' ORDER BY emp_id DESC LIMIT 24",$conn);       
+        $rows = $result->num_rows;
+        $emp_row = array();
+        for ($i = 0; $i < $rows; $i++) {
+            $row = mysqli_fetch_array($result);
+            array_push($emp_row, array('emp_dep'=>$row['emp_dep'],'emp_no'=>$row['emp_no'],'emp_name'=>$row['emp_name']));
+        }
+        echo json_encode($emp_row);
+    }
+
+    //emp_insert_input：手動報到刷卡頁面手動報到insert
+    if ($action == 'emp_insert_input'){
+        $emp_no = $_POST['emp_no'];
+        $emp_name = $_POST['emp_name'];
+        $emp_dep = $_POST['emp_dep'];
+        $result=sql_function("INSERT INTO emplist(emp_no, emp_name, emp_dep, emp_ckin_type) VALUES ('$emp_no', '$emp_name', '$emp_dep', 'F')",$conn);       
+    }
+
+    /* -----list-----*/
+    if ($action == 'emp_list'){
+        $result=sql_function("SELECT * FROM `emplist` WHERE emp_lottery <> '0' ORDER BY emp_lottery DESC",$conn);        
+        $rows = $result->num_rows;
+        $emp_row = array();   
+        for ($i = 0; $i < $rows; $i++) {
+            $row = mysqli_fetch_array($result);
+            array_push($emp_row, array('emp_dep'=>$row['emp_dep'],'emp_no'=>$row['emp_no'],'emp_name'=>$row['emp_name']));
+        }
+        echo json_encode($emp_row);
+    }
+
+
+
+    $conn -> close();  
+}else{
+    echo "不要亂執行！";
+}
+?>
