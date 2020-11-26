b1d08228a2dcc70138ebf4a220b1d4f7b299e48b
0624006 <55527041+0624006@users.noreply.github.com>
2020-11-26 10:59:34 +0800

    Update list_20201119.js

--- js_ajax/list_20201119.js
+++ js_ajax/list_20201119.js
@@ -8,24 +8,24 @@ var dt = "";
 var time = "";
 var refreshSetTimeouId = "";
 var delay = "";
-var list_data="";
+var list_data = "";
 
 $(function () {
-     
+
     dt = new Date();
     time = (dt.getHours() < 10 ? "0" : "") + dt.getHours() + ":" + (dt.getMinutes() < 10 ? "0" : "") + dt.getMinutes() + ":" + (dt.getSeconds() < 10 ? "0" : "") + dt.getSeconds();
     $('#btn_bottom>p').html("— 更新時間：" + time + " —");
 
-    
+
     list();
     //顯示資料，上面可不理。
     function show_list() {
         clearTimeout(refreshSetTimeouId);
         $('#boardbg').empty();
-        
+
         row = Math.ceil(n / 9);
         count = 9;
-        delay=0;
+        delay = 0;
         emp_num = 0;
         dt = new Date();
         time = (dt.getHours() < 10 ? "0" : "") + dt.getHours() + ":" + (dt.getMinutes() < 10 ? "0" : "") + dt.getMinutes() + ":" + (dt.getSeconds() < 10 ? "0" : "") + dt.getSeconds();
@@ -41,9 +41,9 @@ $(function () {
             if (j + 1 === row) {
                 count = n - (row - 1) * count;
             }
-            
+
             for (var i = 0; i < count; i++) {
-                
+
                 list_emp = `獎項：X獎-$1000 <br>部門：${list_data[emp_num].emp_dep}<br>工號：${list_data[emp_num].emp_no}<br>姓名：${list_data[emp_num].emp_name}`;//** 替換資料值，emp_num是用來對應ajax回傳之陣列
                 if (j === 0) {
                     if (i < 5) {
@@ -59,13 +59,14 @@ $(function () {
             delay += 5;
         };
 
+        if (delay == 0) delay = 3; // 當資料庫都沒有獎序時，設定每3秒呼叫資料
+
         refreshSetTimeouId = setTimeout(function () {
-            console.log(delay);
             list();
         }, delay * 1000);
 
-    };  
-    
+    };
+
     // _ajax_新增人員進資料庫
     function list() {
         $.ajax({
@@ -77,8 +78,9 @@ $(function () {
                 'action': 'emp_list',
             },
             success: function (data) {
-                list_data=data;
+                list_data = data;
                 n = Object.keys(data).length;
+                console.log(n);
                 show_list();
             },
             error: function (xhr) { alert("發生錯誤: " + xhr.status + " " + xhr.statusText); }
