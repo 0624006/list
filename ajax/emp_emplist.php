324dbc4c205c63061234b1bf046110d98e3b0732
0624006 <55527041+0624006@users.noreply.github.com>
2020-11-26 11:11:14 +0800

    Update emp_emplist.php

--- ajax/emp_emplist.php
+++ ajax/emp_emplist.php
@@ -3,8 +3,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $action = $_POST['action'];
     require_once('connection.php');
 
-    function sql_function($sql, $conn){     
-        $result=$conn->query($sql);
+    function sql_function($sql, $conn, $mult){
+        if($mult){
+            $result=$conn->mysqli_multi_query($sql); // 多條sql用
+        }else{
+            $result=$conn->query($sql); // 單條sql用
+        }
         if (mysqli_connect_errno($result)) {die("連線失敗: " . mysqli_connect_errno($result));};
         return $result;
     }
@@ -12,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     /* -----check_in-----*/
     //emp_selectAll：報到刷卡頁面顯示已報到人員名單
     if ($action == 'emp_selectAll'){
-        $result=sql_function("SELECT * FROM emplist WHERE emp_ckin_type='N' ORDER BY emp_id DESC LIMIT 40",$conn);        
+        $result=sql_function("SELECT * FROM emplist WHERE emp_ckin_type='N' ORDER BY emp_id DESC LIMIT 40",$conn, false);        
         $rows = $result->num_rows;
         $emp_row = array();        
         for ($i = 0; $i < $rows; $i++) {
@@ -26,13 +30,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         $emp_no = $_POST['emp_no'];
         $emp_name = $_POST['emp_name'];
         $emp_dep = $_POST['emp_dep'];
-        $result=sql_function("INSERT INTO emplist(emp_no, emp_name, emp_dep) VALUES ('$emp_no', '$emp_name', '$emp_dep')",$conn);
+        $result=sql_function("INSERT INTO emplist(emp_no, emp_name, emp_dep) VALUES ('$emp_no', '$emp_name', '$emp_dep')",$conn, false);
     }
 
     /* -----check_in_input-----*/
     //emp_selectALL_input：手動報到頁面顯示已手動報到人員名單
     if ($action == 'emp_selectALL_input'){    
-        $result=sql_function("SELECT * FROM emplist WHERE emp_ckin_type='F' ORDER BY emp_id DESC LIMIT 24",$conn);       
+        $result=sql_function("SELECT * FROM emplist WHERE emp_ckin_type='F' ORDER BY emp_id DESC LIMIT 24",$conn, false);       
         $rows = $result->num_rows;
         $emp_row = array();
         for ($i = 0; $i < $rows; $i++) {
@@ -47,12 +51,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         $emp_no = $_POST['emp_no'];
         $emp_name = $_POST['emp_name'];
         $emp_dep = $_POST['emp_dep'];
-        $result=sql_function("INSERT INTO emplist(emp_no, emp_name, emp_dep, emp_ckin_type) VALUES ('$emp_no', '$emp_name', '$emp_dep', 'F')",$conn);       
+        $result=sql_function("INSERT INTO emplist(emp_no, emp_name, emp_dep, emp_ckin_type) VALUES ('$emp_no', '$emp_name', '$emp_dep', 'F')",$conn, false);       
     }
 
     /* -----list-----*/
     if ($action == 'emp_list'){
-        $result=sql_function("SELECT * FROM `emplist` WHERE emp_lottery <> '0' ORDER BY emp_lottery DESC",$conn);        
+        $result=sql_function("SELECT * FROM `emplist` WHERE emp_lottery <> '0' ORDER BY emp_lottery DESC",$conn, false);        
         $rows = $result->num_rows;
         $emp_row = array();   
         for ($i = 0; $i < $rows; $i++) {
